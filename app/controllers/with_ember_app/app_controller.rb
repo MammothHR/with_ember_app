cdc124dd526db10ba528579816ce347bbeef0b68
Daniel Fockler <danielf@mammothhr.com>
2018-10-09 09:01:59 -0700

    Allow the controller to skip CSRF check

--- app/controllers/with_ember_app/app_controller.rb
+++ app/controllers/with_ember_app/app_controller.rb
@@ -1,5 +1,6 @@
 module WithEmberApp
   class AppController < ApplicationController
+    skip_before_action :verify_authenticity_token
     skip_before_action :authenticate_user!
     before_action :authenticate_deploy!, only: [:create]
 
